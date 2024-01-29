e72b5a8b79c9ce1c304c780ca101450d7fc5ca81
raul149 <72709781+raul149@users.noreply.github.com>
2024-01-29 18:12:01 +0100

    Update prod.py

--- portfolio/settings/prod.py
+++ portfolio/settings/prod.py
@@ -24,9 +24,6 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CSRF_TRUSTED_ORIGINS = ['https://*.127.0.0.1', os.environ.get("HOST_NAME"),'raulaznar.onrender.com']
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
-DATABASES["default"] = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
-
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
