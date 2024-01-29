f1082d4b9973256f4dd0da081057b06c189e410f
raul149 <72709781+raul149@users.noreply.github.com>
2024-01-29 13:05:18 +0100

    Update prod.py

--- portfolio/settings/prod.py
+++ portfolio/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 import dj_database_url
 
 DEBUG = False
-
+ALLOWED_HOSTS = ['127.0.0.1', 'raulaznaralvarez.onrender.com']
 INSTALLED_APPS += [
     'whitenoise.runserver_nostatic',
     'cloudinary',
