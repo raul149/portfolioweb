7983728336b00ef1794177216879eaeaf100defd
raul149 <72709781+raul149@users.noreply.github.com>
2024-01-29 13:34:21 +0100

    Update prod.py

--- portfolio/settings/prod.py
+++ portfolio/settings/prod.py
@@ -5,7 +5,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = []
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')if RENDER_EXTERNAL_HOSTNAME:    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)INSTALLED_APPS += [    'whitenoise.runserver_nostatic',
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')if RENDER_EXTERNAL_HOSTNAME:    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+INSTALLED_APPS += [    'whitenoise.runserver_nostatic',
     'cloudinary',
     'cloudinary_storage',
 ]
