3984b6c291ce948ba6eadea98e57cd52c84e550a
raul149 <72709781+raul149@users.noreply.github.com>
2024-01-30 19:21:01 +0100

    Update base.py

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
+import dj_database_url
 from django.core.management.utils import get_random_secret_key
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
