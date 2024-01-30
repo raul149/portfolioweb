da629678e45cdf1573e71912b7c3f8ebe6d7ebcf
raul149 <72709781+raul149@users.noreply.github.com>
2024-01-30 19:35:10 +0100

    Update wsgi.py

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -15,5 +15,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings.prod")
 
 application = get_wsgi_application()
 
-User = get_user_model();
-User.objects.create_superuser('admin', 'admin@gmail.com', 'password')
+#User = get_user_model();
+#User.objects.create_superuser('admin', 'admin@gmail.com', 'password')
